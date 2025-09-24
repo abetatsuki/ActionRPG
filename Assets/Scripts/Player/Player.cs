d62ac82d7049e4aaef142059a990aebeea9b30b6
kenkatou354 <kenkatou354@gmail.com>
2025-09-25 01:39:06 +0900

    [Add]ゲームマネージャーからプレイヤーのインベントリの初期化をできるように

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private PlayerInventory inventory = new PlayerInventory();
+    [SerializeField] private PlayerInventory inventory;
+
+    public void InitializeForNewGame()
+    {
+        inventory = new PlayerInventory();
+    }
+
+    public void LoadInventory(PlayerInventory loadedData)
+    {
+        inventory = loadedData;
+    }
+
+    // もしインスペクターで見たい場合、プロパティで公開
     public PlayerInventory Inventory => inventory;
 }
