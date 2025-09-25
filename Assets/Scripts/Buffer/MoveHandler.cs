25067cba110e46f0939c0646ecd8f69089a3bb1e
kenkatou354 <kenkatou354@gmail.com>
2025-09-25 16:32:59 +0900

    [Add]プレイヤーが走るアニメーションとスプリント切り替えを実装

--- Assets/Scripts/Buffer/MoveHandler.cs
+++ Assets/Scripts/Buffer/MoveHandler.cs
@@ -5,19 +5,26 @@ public class MoveHandler : MonoBehaviour
     [SerializeField] private PlayerInputNotifier _notifier;
     [SerializeField] private Status _status;
     [SerializeField] private Rigidbody _rb;
-    [SerializeField] private float _basespeed = 0f;
-    [SerializeField] private float _sprintspeed = 0f;
+    [SerializeField] private float _basespeed = 3f;
+    [SerializeField] private float _sprintspeed = 6f;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform _cameraTransform;
 
-    [SerializeField] private Transform _cameraTransform; // ★追加: カメラの向きを参照する用
+    [SerializeField] private float _currentSpeed; 
+
+    private bool _isSprinting = false;
+
 
     private void OnEnable()
     {
         _notifier.OnMove += _status.SetMoveInput;
+        _notifier.OnSprint += SetSprint;
     }
 
     private void OnDisable()
     {
         _notifier.OnMove -= _status.SetMoveInput;
+        _notifier.OnSprint -= SetSprint;
     }
 
     private void FixedUpdate()
@@ -25,14 +32,17 @@ public class MoveHandler : MonoBehaviour
         MovePlayer();
     }
 
+    private void SetSprint(bool sprinting)
+    {
+        _isSprinting = sprinting;
+    }
+
     private void MovePlayer()
     {
         Vector2 input = _status.MoveInput;
 
-        // ★ プレイヤーの向きではなくカメラの向きを使う
         Vector3 forward = _cameraTransform.forward;
         Vector3 right = _cameraTransform.right;
-
         forward.y = 0f;
         right.y = 0f;
         forward.Normalize();
@@ -40,9 +50,20 @@ public class MoveHandler : MonoBehaviour
 
         Vector3 moveDir = (forward * input.y + right * input.x).normalized;
 
-        float speed = 3f;
+        if (input.magnitude > 0)
+        {
+            _currentSpeed = _isSprinting ? _sprintspeed : _basespeed;
+        }
+        else
+        {
+            _currentSpeed = 0f;
+        }
 
-        Vector3 velocity = moveDir * speed;
+
+        Vector3 velocity = moveDir * _currentSpeed;
         _rb.linearVelocity = new Vector3(velocity.x, _rb.linearVelocity.y, velocity.z);
+
+
+        _animator.SetFloat("Speed", _currentSpeed);
     }
 }
